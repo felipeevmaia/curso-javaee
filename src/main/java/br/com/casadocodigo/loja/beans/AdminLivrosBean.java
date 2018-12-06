98441c39a245d09cced335b307fe86015dd6ba96
Felipe Maia <felipe.viveiros@gmail.com>
2018-12-06 12:15:32 -0300

    Inclusão de persistência de dados

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,14 +1,50 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.casadocodigo.loja.daos.AutorDao;
+import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
+@Named
+@RequestScoped
 public class AdminLivrosBean {
-	private Livro livro;
+	private Livro livro = new Livro();
 	
+	@Inject
+	private LivroDao dao;
+	
+	@Inject
+	private AutorDao autorDao;
+	
+	private List<Integer> autoresId = new ArrayList<>();
+	
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
+	}
+
 	public void salvar() {
+		for (Integer id : autoresId) {
+			livro.getAutores().add(new Autor(id));
+		}
+		dao.salvar(livro);
 		System.out.println("Livro :" + livro + " cadastrado com Sucesso!");
 	}
 
+	public List<Autor> getAutores(){
+		return autorDao.getLista();
+	}
+	
 	public Livro getLivro() {
 		return livro;
 	}
--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -1,12 +1,34 @@
 package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Livro {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
 	private String titulo;
 	private String descricao;
 	private BigDecimal preco;
 	private Integer numeroPaginas;
+	
+	@ManyToMany
+	public List<Autor> autores;
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
 
 	public String getTitulo() {
 		return titulo;
